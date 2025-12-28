d8a9328c76582e9da01d0588b330dec737b46d37
unknown <thulasiraman763@gmail.com>
2025-12-28 22:49:37 +0530

    Print

--- January/Hello.py
+++ January/Hello.py
@@ -1,4 +1,12 @@
-print("Hello, I'm Thulasiraman. Here i'll share my documentation of learning Python.")
+print("Hello, I'm Thulasiraman. Here i'll share my documentation of learning Python.") 
 
 print("hello world")
 print("This is my first python program.")
+print("Hi, \"python\" is the language I'm learning now.") 
+print("Hi, 'python' is the language I'm learning now.") #Using single quotes
+print("C:\\thulasiraman\\python") #Using escape sequence for backslash
+
+print("Thulasi\nraman") #Using escape sequence for new line
+print("Thulasi\tramam") #Using escape sequence for tab
+
+print("Your Learning Path:\n\t- PythonBasics\n\t-data Engineering\n\t-AI")
