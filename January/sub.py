7da5ef9646950a7bba630af8789a79131f210045
unknown <thulasiraman763@gmail.com>
2025-12-28 16:32:56 +0530

    add sub

---
+++ January/sub.py
@@ -0,0 +1,5 @@
+# Addition of two numbers its very basic, but still I'm testing my git skills
+a=int(input("Enter no 1:"))
+b=int(input("Enter no 2:"))
+output=a-b
+print("The difference is:",output)
